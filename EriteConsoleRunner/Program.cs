0d7cefa1aba283420496d2b765f96413cd846094
kakkaroolari <homorunkkari2@hotmail.com>
2019-02-14 21:18:44 +0200

    added mab reader

--- EriteConsoleRunner/Program.cs
+++ EriteConsoleRunner/Program.cs
@@ -15,7 +15,11 @@ namespace EriteConsoleRunner
 
             Console.WriteLine($"Maanvaraisen laatan johtumishavio: {test.MaanvaraisenLaatanJohtumishavio()}");
             Console.WriteLine($"Vuotoilman lammitysenergia: {test.VuotoilmanLammitysenergia()} kWh");
-            
+
+
+            var mabReader = new EriteLib.MabReader();
+            var byteArray = new byte[] { 0x05, 0x2B, 0x00, 0x00, 0x65, 0x00, 0x00, 0x00, 0xAC, 0x08, 0x00, 0x00, 0x25, 0x00, 0x07, 0x00, 0x42, 0x65, 0x74, 0x6F, 0x6E, 0x69, 0x00 };
+            mabReader.TryDecode(byteArray);
         }
     }
 }
---
+++ EriteLib/MabReader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace EriteLib
+{
+    public class MabReader
+    {
+        public void TryDecode(byte[] binaryData)
+        {
+            // data 00 terminator
+            int bytesRead = 0;
+            var buffer = new byte[100]; //< todo: size
+            for(int i=0; i<binaryData.Length; i++)
+            {
+                if (bytesRead == 0 || (bytesRead != 0 && binaryData[i] != 0x0))
+                {
+                    buffer[bytesRead] = binaryData[i];
+                    bytesRead++;
+                }
+                else
+                {
+                    // terminator encountered
+                    var output = BitConverter.ToInt32(buffer, 0);
+                    Debug.WriteLine($"[ERITE] index-{i}: hex:{ByteArrayToString(buffer)}, {output}");
+                    Array.Clear(buffer, 0, 100);
+                    bytesRead = 0;
+                }
+            }
+        }
+
+        private static string ByteArrayToString(byte[] ba)
+        {
+            StringBuilder hex = new StringBuilder(ba.Length * 2);
+            int zeros = 0;
+            foreach (byte b in ba)
+            {
+                hex.AppendFormat("{0:x2}", b);
+                if (0x0 == b) zeros++;
+                if (2 == zeros) break;
+            }
+            return hex.ToString();
+        }
+    }
+}
