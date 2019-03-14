594be2e40081c8ce549b3cfc5b7c0ea9a484fd7e
fivanl <ville.lehtisaari@sweco.fi>
2019-03-14 14:17:10 +0200

    Add SkiaWpf example as UI for protoing.

---
+++ EriteLampo/View/MainView.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+//using Windows.UI.Xaml;
+
+namespace SkiaWpf.View
+{
+  /// <summary>
+  /// Interaction logic for MainView.xaml
+  /// </summary>
+  public partial class MainView : UserControl
+  {
+    public MainView()
+    {
+      InitializeComponent();
+    }
+
+      private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+      {
+          // the the canvas and properties
+          var canvas = e.Surface.Canvas;
+
+            // get the screen density for scaling
+            //var display = DisplayInformation.GetForCurrentView();
+            //double dpi = DisplayProperties.LogicalDpi;
+            //var display = Window.GetWindow()
+          PresentationSource source = PresentationSource.FromVisual(this);
+
+          double dpiX, dpiY;
+          float scale = 1;
+          if (source != null)
+          {
+              scale = ToSingle(source.CompositionTarget?.TransformToDevice.M11);
+              //dpiX = 96.0 * source.CompositionTarget.TransformToDevice.M11;
+              //dpiY = 96.0 * source.CompositionTarget.TransformToDevice.M22;
+          }
+
+          //  var scale = display.LogicalDpi / 96.0f;
+          //var scale = 
+          var scaledSize = new SKSize(e.Info.Width / scale, e.Info.Height / scale);
+
+          // handle the device screen density
+          canvas.Scale(scale);
+
+          // make sure the canvas is blank
+          canvas.Clear(SKColors.White);
+
+          // draw some text
+          var paint = new SKPaint
+          {
+              Color = SKColors.Black,
+              IsAntialias = true,
+              Style = SKPaintStyle.Fill,
+              TextAlign = SKTextAlign.Center,
+              TextSize = 24
+          };
+          var coord = new SKPoint(scaledSize.Width / 2, (scaledSize.Height + paint.TextSize) / 2);
+          canvas.DrawText("SkiaSharp", coord, paint);
+      }
+
+      public static float ToSingle(double? value)
+      {
+          if (null == value) return 1;
+          return (float)value;
+      }
+    }
+}
---
+++ EriteLampo/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using SkiaWpf.Service;
+using System.Windows.Media.Imaging;
+
+namespace SkiaWpf.ViewModel
+{
+  public interface IMainViewModel : INotifyPropertyChanged
+  {
+    string Name { get; }
+    ImageSource ImageSource { get; }
+  }
+
+  public class MainViewModel : BaseViewModel, IMainViewModel
+  {
+    #region ImageSource
+    private ImageSource _ImageSource;
+    public ImageSource ImageSource
+    {
+      get { return _ImageSource; }
+      set
+      {
+        if (_ImageSource != value)
+        {
+          _ImageSource = value;
+          OnPropertyChanged(nameof(ImageSource));
+        }
+      }
+    }
+    #endregion
+
+    public string Name { get; private set; }
+
+    private readonly IImageService ImageService;
+    private readonly WriteableBitmap WriteableBitmap;
+
+    public MainViewModel(IImageService imageService)
+    {
+      if (imageService == null)
+        throw new ArgumentNullException(nameof(imageService));
+
+      this.Name = "SkiaSharp Wpf Example";
+
+      this.ImageService = imageService;
+      this.ImageSource = this.WriteableBitmap = imageService.CreateImage(900, 600);
+      CompositionTarget.Rendering += (o, e) => this.ImageService.UpdateImage(this.WriteableBitmap);
+    }
+
+  }
+}
